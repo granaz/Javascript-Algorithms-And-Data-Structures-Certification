4d990c9d651e2086386b2ee7c2aab9d76cc88b49
Henrique Grana <hgrana@daitan.com>
2019-01-04 16:02:08 -0200

    Symm Diff

---
+++ algorithms/symmetric-diff.js
@@ -0,0 +1,23 @@
+function sym(args) {
+  var pre = [...arguments];
+
+  return pre.reduce(diff);
+
+  function diff(arrTo, arrFrom) {
+    let arr1, arr2, result;
+
+    arr1 = arrTo.filter(item => {
+      return !(arrFrom.indexOf(item) > -1);
+    });
+
+    arr2 = arrFrom.filter(item => {
+      return !(arrTo.indexOf(item) > -1);
+    });
+
+    result = arr1.concat(arr2);
+
+    return result.filter((item, index, self) => {
+      return index == self.indexOf(item);
+    });
+  }
+}
